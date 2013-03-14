5fcc4ae2b6ff66921fe11771c6ba0ad30e15229f
Nilesh Ashra <nilesh@ashra.co.uk>
2013-03-13 23:27:52 -0700

    now using mp4

--- examples.py
+++ examples.py
@@ -2,5 +2,5 @@ from videostitch import stitch
 
 
 clips = [open('test_videos/1.mpg'), open('test_videos/2.mpg')]
-combined = stitch(clips, output="/tmp/sss.mpg")
+combined = stitch(clips, output="/tmp/sss.mp4")
 # combined is  file descriptor
--- videostitch.py
+++ videostitch.py
@@ -10,5 +10,5 @@ def stitch(videos, output):
 		for v in videos:
 			yield v.read()
 
-	ffmpeg("-q:v", "0", output, y=True, i="pipe:0", r=25, _in=feed(), _in_bufsize=1024)
+	ffmpeg("-q:v", "0", '-vcodec', 'libx264', '-acodec', 'libfaac', output, y=True, i="pipe:0", r=25, _in=feed(), _in_bufsize=1024)
 	return open(output, 'r')
