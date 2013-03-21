ac8ae4d0f57f13d75771e09f23028244b7bd8f5e
Grant Thomas <jgrantthomas@gmail.com>
2013-03-21 08:43:26 -0700

    some operations require ffmpeg to operate on the whole file at once (no pipes allowed) -- change normalize and change_frame_rate to allow for file names. Other functions should receive mpeg videos as input.

--- videostitch.py
+++ videostitch.py
@@ -4,23 +4,40 @@ from cStringIO import StringIO
 from sh import ffmpeg, ffprobe, wc, ls
 from Queue import Queue
 
+
+def _probe(video_file):
+    if type(video_file) == file:
+        out = ffprobe("pipe:0", _in=feed(), _in_bufsize=1024).stderr
+    else:
+        out = ffprobe(video_file)
+    return out.stderr
+        
+
 def normalize(video_file, output):
     """
     Transcode the video to mpeg.
     """
     def feed():
         return video_file.read()
-    ffmpeg("-q:v", "0", output, y=True, i="pipe:0", r=25, _in=feed(), _in_bufsize=1024)
+
+    if type(video_file) == file:
+        ffmpeg("-y", "-q:v", "0", output, i="pipe:0", r=25, _in=feed(), _in_bufsize=1024)
+    else:
+        ffmpeg("-y", "-q:v", "0", output, i=video_file, r=25)
     return open(output, 'r')
 
-def normalize_frame_rate(video_file, output, fps=24):
+def change_frame_rate(video_file, output, fps=24):
     """
     Normalize the frame rate of the input file
     """
     def feed():
         return video_file.read()
     frame_rate = get_frame_rate(video_file)
-    ffmpeg("-r", frame_rate, "-i", "pipe:0", "-r", "24", output, _in=feed(), _in_bufsize=1024, y=True)
+
+    if type(video_file) == file:
+        ffmpeg("-r", frame_rate, "-i", "pipe:0", "-r", "24", output, _in=feed(), _in_bufsize=1024, y=True)
+    else:
+        ffmpeg("-r", frame_rate, "-i", output, "-r", "24", output, y=True)
     return open(output, 'r')
 
 def get_frame_rate(video_file):
@@ -32,8 +49,8 @@ def get_frame_rate(video_file):
         return video_file.read()
     frame_rate = 0
     pattern = re.compile(r'([0-9]{1,2}(\.[0-9]{1,2})?) fps')
-    out = ffprobe("pipe:0", _in=feed(), _in_bufsize=1024).stderr
-    print out
+
+    out = _probe(video_file)
     matches = pattern.search(out)
     if matches:
         try:            
@@ -52,7 +69,8 @@ def get_dimensions(video_file):
     def feed():
         return video_file.read()
     pattern = re.compile(r'([0-9]{3,4})x([0-9]{3,4}),')
-    out = ffprobe("pipe:0", _in=feed(), _in_bufsize=1024).stderr
+
+    out = _probe(video_file)
     matches = pattern.search(out)
     if matches:
         width, height = map(int, matches.groups()[0:2])
@@ -102,7 +120,16 @@ def to_mp4(video_file, output):
     """
     def feed():
         return video_file.read()
-    ffmpeg("-i", "pipe:0", "-sameq", "-vcodec libx264", "-acodec libfaac", output, _in=feed(), _in_bufsize=1024)
+    ffmpeg("-i", "pipe:0", "-sameq", "-vcodec libx264", "-acodec libmp3lame", output, _in=feed(), _in_bufsize=1024)
+    return open(output, 'r')
+
+def resize(video_file, output, dimensions=(360,360)):
+    """
+    Resize the video to the given dimensions
+    """
+    def feed():
+        return video_file.read()
+    ffmpeg("-i", "pipe:0", "-s", "%sx%s" % dimensions, output, y=True, _in=feed(), _in_bufsize=1024)
     return open(output, 'r')
     
 def stitch(videos, output):
@@ -120,6 +147,35 @@ def stitch(videos, output):
     return open(output, 'r')
 
 
-DEFAULT_OPERATIONS = [
-    
+def stitch_theora(videos, output):
+    """
+    Stitch together the video files in the iterable 'videos'    
+    """
+    def feed():
+        """
+        Feed the video streams using a generator to avoid
+        in-memory concat of all the streams
+        """
+        for v in videos:
+            yield v.read()
+    ffmpeg("-q:v", "0", '-vcodec', 'libtheora', '-acodec', 'libvorbis', output, y=True, i="pipe:0", r=25, _in=feed(), _in_bufsize=1024)
+    return open(output, 'r')
+
+
+DEFAULT_PROCESSORS = [
+    change_frame_rate,
+    crop_square,
+    resize
     ]
+
+
+def process_video(video_file, output, processors=DEFAULT_PROCESSORS):
+    """
+    Run the processors in the iterable processsors
+    """
+    current_file = video_file
+    def feed():
+        current_file.read()
+    for function in processors:
+        current_file = function(current_file, output)
+    return open(output, 'r')
